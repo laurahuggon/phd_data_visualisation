9e634ff849deac3e1f02d0c98b37b72e9dca5c32
laurahuggon <117913317+laurahuggon@users.noreply.github.com>
2025-01-27 15:26:48 +0000

    Update plot_wb_i3neuron_synaptosome_validation.R

--- western_blots/plot_wb_i3neuron_synaptosome_validation.R
+++ western_blots/plot_wb_i3neuron_synaptosome_validation.R
@@ -7,9 +7,9 @@ library(readxl)
 # Define variables --------------------------------------------------------
 
 parent_filepath = "/Users/laurahuggon/Library/CloudStorage/OneDrive-King'sCollegeLondon/phd/lab/wb/i3neuron_synapse/synper_extraction_practice/"
-relative_filepath = "unc/"
-filename = "empiria_unc.xlsx"
-protein_name = "UNC13A"
+relative_filepath = "stx/"
+filename = "empiria_stx.xlsx"
+protein_name = "Syntaxin-1A"
 
 colour1 = "grey40"
 colour2 = "grey88"
@@ -41,136 +41,8 @@ empiria_data$Fraction = factor(empiria_data$Fraction, levels = c("Homogenate", "
 
 # Remove homogenate
 empiria_data = empiria_data %>%
-  filter(Fraction != "Homogenate")
-
-
-# # Find group means --------------------------------------------------------
-# 
-# # Calculate mean for each group
-# group_means = empiria_data %>%
-#   group_by(Replicate) %>%
-#   summarise(
-#     N = n(), # Count the number of samples in each group
-#     Group_Mean = mean(`Normalized Signal`), # Calculate mean for each group
-#     )
-#  
-# 
-
-# # Normalise to homogenate
-# empiria_data = empiria_data %>%
-#   group_by(Replicate) %>%
-#   mutate(foldchange = abs(`Normalized Signal` / `Normalized Signal`[Fraction=="Homogenate"]))
-
-# Reverse the foldchange signals except for the value of 1
-# empiria_data <- empiria_data %>%
-#   mutate(foldchange = ifelse(foldchange != 1, -foldchange, foldchange))
-# 
-# # Export .csv
-# write.csv(normalised_to_control, paste0(parent_filepath, relative_filepath, protein_name, "_normalised_to_control.csv"), row.names=FALSE)
-# 
-# # Find sd using normalised (to control) values
-# sd_values = normalised_to_control %>%
-#   group_by(Replicate) %>%
-#   summarise(
-#     SD = sd(Normalised_to_Control)
-#     )
-# 
-# # Merge `sd_values` with `group_means`
-# group_means = group_means %>%
-#   inner_join(sd_values, by = "Replicate")
-# 
-# 
-# # Statistics --------------------------------------------------------------
-# 
-# # Create function to test normality, equal variance, and perform appropriate statistical test
-# perform_test = function(data) {
-#   
-#   # Extract values grouped by Replicate using Normalised_to_Control
-#   # Test for normality using Shapiro-Wilk test
-#   normality_result = by(data$Normalised_to_Control, data$Replicate, shapiro.test)
-#   
-#   wt_normal = normality_result$WT$p.value > 0.05 # If p-value is greater than 0.05, the data is considered normally distributed
-#                                                  # `wt_normal` is set to `TRUE` if p-value is greater than 0.05
-#   q331k_normal = normality_result$Q331K$p.value > 0.05 # If p-value is greater than 0.05, the data is considered normally distributed
-#                                                        # `qk_normal` is set to `TRUE` if p-value is greater than 0.05
-#   
-#   if (!wt_normal || !q331k_normal) { # If either variable is set to `FALSE`
-#     # If data is not normally distributed, perform Mann-Whitney test
-#     message = "Data not normally distributed; perform Mann-Whitney test"
-#     test_result = wilcox.test(Normalised_to_Control ~ Replicate, data = data) # `Normalised_to_Control ~ Replicate` specifies that you want to compare values in the `Normalised_to_Control` column grouped by `Replicate`
-#   } else {
-#     # Test for equal variances using F-test
-#     variance_test = var.test(Normalised_to_Control ~ Replicate, data = data)
-#     var_equal = variance_test$p.value > 0.05 # If p-value is greater than 0.05, it assumes both replicates have equal variances
-#     
-#     if (!var_equal) { # If `var_equal` variable is set to `FALSE`
-#       # If variances are not equal, perform t-test with var.equal = FALSE
-#       message = "Data does not have equal variance; perform Welch's t-test"
-#       test_result = t.test(Normalised_to_Control ~ Replicate, data = data, var.equal = FALSE)
-#     } else {
-#       # If variances are equal, perform t-test with var.equal = TRUE
-#       message = "Data has equal variance; perform Student's t-test"
-#       test_result = t.test(Normalised_to_Control ~ Replicate, data = data, var.equal = TRUE)
-#     }
-#   }
-#   
-#   # Return both test results and the message
-#   return(list(test_result = test_result, message = message))
-# }
-# 
-# # Perform t-test
-# result = perform_test(normalised_to_control)
-# 
-# # Accessing the test results
-# test_result = result$test_result
-# 
-# # Accessing the message
-# message = result$message
-# 
-# # Create function that converts p-values to signficance stars and generates a dataframe of annotations
-# prepare_annotations = function(test_result) {
-#   # Convert p-values to stars based on traditional significance levels
-#   convert_p_to_stars = function(p_value) {
-#     if (p_value <= 0.0001) {
-#       return("****")
-#     } else if (p_value <= 0.001) {
-#       return("***")
-#     } else if (p_value <= 0.01) {
-#       return("**")
-#     } else if (p_value <= 0.05) {
-#       return("*")
-#     } else {
-#       return("")  # Not significant
-#     }
-#   }
-#   
-#   # Extract p-value from test results
-#   p_value = test_result$p.value
-#   
-#   # Convert p-value to stars
-#   stars = convert_p_to_stars(p_value)
-#   
-#   # Create a dataframe `annotations`
-#   annotations = data.frame(
-#     p_value = p_value,
-#     Stars = stars,
-#     stringsAsFactors = FALSE
-#   )
-#   
-#   # Filter out non-significant annotations
-#   annotations = annotations[annotations$Stars != "", ]
-#   
-#   return(annotations)
-# }
-# 
-# # Create annotations 
-# annotation = prepare_annotations(test_result)
-# 
-# # Find the maximum y-values -> this is for dynamic annotation bars in the plots
-# annotation = annotation %>%
-#   mutate(
-#     max_y = max(group_means$Normalised_Group_Mean + group_means$SD)
-#   )
+  filter(Fraction != "Homogenate") %>%
+  select(Lane, Replicate, Fraction, MW, `Normalized Signal`)
 
 
 # Data visualisation ------------------------------------------------------
@@ -192,7 +64,7 @@ my_theme_facet = function() {
 }
 
 # Create a function that takes two dataframes and column names to generate multiple bar plots with overlayed data points
-plot_normalised = function(empiria_data, x = "Fraction", y = "`Normalized Signal`") {
+plot_normalised = function(data, x = "Fraction", y = "`Normalized Signal`") {
   
   # Calculate the maximum y value to set upper axis limit
   max_y_value = max(empiria_data$`Normalized Signal`, na.rm = TRUE)
@@ -219,7 +91,9 @@ plot_normalised = function(empiria_data, x = "Fraction", y = "`Normalized Signal
     
     # Plot appearance
     my_theme_facet() +
-    scale_y_continuous(limits = c(0, upper_limit), expand = c(0, 0)) +  # Setting both multiplier and add-on to 0
+    scale_y_continuous(limits = c(0, upper_limit),   # Set the y-axis range
+                       expand = c(0, 0)  # Remove extra space around the axis
+                       ) +
     
     # Overlay individual data points
     geom_point(data = empiria_data, aes_string(x = x,
